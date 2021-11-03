f13ad198608c727d6191a1bb2777ecf7758ce16c
George <george_roots@hotmail.com>
2021-11-03 10:54:54 -0300

    [Feature George - Task: ADD menu component]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PaginaInicialComponent } from './pagina-inicial/pagina-inicial.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PaginaInicialComponent
+    PaginaInicialComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
