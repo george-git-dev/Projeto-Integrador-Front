61e825d2ac8c8e26877d22f01bf8151e0bc5407a
DanielSanm <danielelias3221@gmail.com>
2021-11-18 11:54:25 -0300

    [Feature Daniel - inserindo modal no menu]

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -4,6 +4,7 @@ import { environment } from 'src/environments/environment.prod';
 import { Postagem } from '../model/Postagem';
 import { Tema } from '../model/Tema';
 import { Usuario } from '../model/Usuario';
+import { PostagemComponent } from '../postagem/postagem.component';
 import { AuthService } from '../service/auth.service';
 import { PostagemService } from '../service/postagem.service';
 import { TemaService } from '../service/tema.service';
@@ -69,8 +70,6 @@ export class InicioComponent implements OnInit {
       this.usuario = resp
     })
   }
-
-
 
   publicar(){
     this.tema.idTema = this.idTema
